13de4b7056c9f247ba20329b61bb4ef1e16ceff9
xlwas <rlaxoan9327@jj.ac.kr>
2025-06-23 02:10:27 +0900

    일단 간단한 입출력은 구현완

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,24 @@
 package my.contacts
+//import kotlin.io
+import java.io.File
+
+val file_path_resources: String = "src/main/resources/"
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    input_data()
+    print_data()
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun input_data() {
+    val file = File(file_path_resources+"contacts_list.txt")
+    val data = readln()
+    file.appendText(data+'\n')
+}
+
+fun print_data() {
+    val file = File(file_path_resources+"contacts_list.txt")
+    val content: String = file.readText()
+    println(content)
 }
